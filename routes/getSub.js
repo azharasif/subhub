ac6762cb12d50b02f237ee764ef6bfb514f2bf05
azhar_asif <muhammad.azhar@funavry.com>
2020-10-09 20:46:47 +0500

    new

--- routes/getSub.js
+++ routes/getSub.js
@@ -5,32 +5,32 @@ const crypto = require('crypto');
 const router = express.Router();
 const functions = require('../middleware/functions')
 const con = require('../db');
-const Schema = Joi.object().keys({
-    userid: Joi.number().integer().required(),
-    subname: Joi.string().required().allow(""),
-    rating: Joi.number().integer().required().allow(""),
-    distance: Joi.number().integer().required().allow(""),
-    price:Joi.number().integer().required().allow(""),
-    lng:Joi.number().integer().required().allow(""),
-    lat:Joi.number().integer().required().allow(""),
-});
+// const Schema = Joi.object().keys({
+//     userid: Joi.number().integer().required(),
+//     subname: Joi.string().required().allow(""),
+//     rating: Joi.number().integer().required().allow(""),
+//     distance: Joi.number().integer().required().allow(""),
+//     price:Joi.number().integer().required().allow(""),
+//     lng:Joi.number().integer().required().allow(""),
+//     lat:Joi.number().integer().required().allow(""),
+// });
 
 router.post('/' , async  function (req, res) {
     try{
-        let validated = Schema.validate(req.body, { abortEarly: false });
-    if (!validated.error) {
+    //     let validated = Schema.validate(req.body, { abortEarly: false });
+    // if (!validated.error) {
 
-                 let  query = (`Select * from jobs where  subname like concat('%',"${req.body.subname}", '%')   and
-                  rating = ${req.body.rating}  and   distance  = ${req.body.distance} and price =  ${req.body.price} and 
-                  lng =  ${req.body.lng} and lat = ${req.body.lat}`);
+                 let  query = (`Select * from jobs where  subname like concat('%',"${req.body.subname}", '%')   or
+                  rating = ${req.body.rating}  or   distance  = ${req.body.distance} or price =  ${req.body.price} or 
+                  lng =  ${req.body.lng} or lat = ${req.body.lat}`);
                   console.log(query)
                  let result =  await functions.runQuery(query);
                  res.send({statusCode:200, data:result})
-                }
-                else {
-                    res.send({ statusCode: 405, message: validated.error.message });
-                }
-    }
+    //             }
+    //             else {
+    //                 res.send({ statusCode: 405, message: validated.error.message });
+    //             }
+ }
     catch(err){
 
         res.send({statusCode:500 , message:err.message})
