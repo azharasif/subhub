e31b3dd981c6be441dc9407630d46b7c28d548dd
azhar_asif <muhammad.azhar@funavry.com>
2020-10-09 20:36:30 +0500

    new

--- routes/getSub.js
+++ routes/getSub.js
@@ -5,13 +5,30 @@ const crypto = require('crypto');
 const router = express.Router();
 const functions = require('../middleware/functions')
 const con = require('../db');
+const Schema = Joi.object().keys({
+    userid: Joi.number().integer().required(),
+    subname: Joi.string().required().allow(""),
+    rating: Joi.number().integer().required().allow(""),
+    distance: Joi.number().integer().required().allow(""),
+    price:Joi.number().integer().required().allow(""),
+    lng:Joi.number().integer().required().allow(""),
+    lat:Joi.number().integer().required().allow(""),
+});
 
-router.get('/' , async  function (req, res) {
+router.post('/' , async  function (req, res) {
     try{
-        
-                 let  query = (`select  *  from jobs`);
+        let validated = userSchema.validate(req.body, { abortEarly: false });
+    if (!validated.error) {
+
+                 let  query = (`Select * from jobs where  subname like concat('%',"${req.body.subname}", '%')   or 
+                  rating = ${req.body.fueltype}  or   distance  = ${req.body.distance} or price =  ${req.body.price} or 
+                  lng =  ${req.body.lng} or lat = ${req.body.lat}`);
                  let result =  await functions.runQuery(query);
                  res.send({statusCode:200, data:result})
+                }
+                else {
+                    res.send({ statusCode: 405, message: validated.error.message });
+                }
     }
     catch(err){
 
