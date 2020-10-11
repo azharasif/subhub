c0e012535816010832d426afbf64d4034bb52bbf
azhar_asif <muhammad.azhar@funavry.com>
2020-10-11 19:00:16 +0500

    new

--- routes/getSub.js
+++ routes/getSub.js
@@ -5,31 +5,31 @@ const crypto = require('crypto');
 const router = express.Router();
 const functions = require('../middleware/functions')
 const con = require('../db');
-// const Schema = Joi.object().keys({
-//     userid: Joi.number().integer().required(),
-//     subname: Joi.string().required().allow(""),
-//     rating: Joi.number().integer().required().allow(""),
-//     distance: Joi.number().integer().required().allow(""),
-//     price:Joi.number().integer().required().allow(""),
-//     lng:Joi.number().integer().required().allow(""),
-//     lat:Joi.number().integer().required().allow(""),
-// });
+const Schema = Joi.object().keys({
+    userid: Joi.number().integer().required().allow(""),
+    subname: Joi.string().required().allow(""),
+    rating: Joi.number().integer().required().allow(""),
+    distance: Joi.number().integer().required().allow(""),
+    price:Joi.number().integer().required().allow(""),
+    lng:Joi.number().integer().required().allow(""),
+    lat:Joi.number().integer().required().allow(""),
+});
 
 router.post('/' , async  function (req, res) {
     try{
-    //     let validated = Schema.validate(req.body, { abortEarly: false });
-    // if (!validated.error) {
+        let validated = Schema.validate(req.body, { abortEarly: false });
+    if (!validated.error) {
 
-                 let  query = (`Select *  , max(distance) from jobs where  subname like concat('%',"${req.body.subname}", '%')   or
+                 let  query = (`Select *   from jobs where  subname like concat('%',"${req.body.subname}", '%')   or
                   rating = "${req.body.rating}"   or price =  "${req.body.price}" or 
                   lng = "${req.body.lng}" or lat =" ${req.body.lat}"`);
                   console.log(query)
                  let result =  await functions.runQuery(query);
                  res.send({statusCode:200, data:result})
-    //             }
-    //             else {
-    //                 res.send({ statusCode: 405, message: validated.error.message });
-    //             }
+                }
+                else {
+                    res.send({ statusCode: 405, message: validated.error.message });
+                }
  }
     catch(err){
 
